a1610dc89017db36e92fcf19ee56365a216b70ed
avasquez.externo <avasquez.externo@sii.cl>
2026-05-12 08:04:03 -0400

    Documentación updated

--- src/main/java/com/example/usersapi/domain/exception/EmailAlreadyRegisteredException.java
+++ src/main/java/com/example/usersapi/domain/exception/EmailAlreadyRegisteredException.java
@@ -2,6 +2,6 @@ package com.example.usersapi.domain.exception;
 
 public class EmailAlreadyRegisteredException extends RuntimeException {
     public EmailAlreadyRegisteredException() {
-        super("El correo ya registrado");
+        super("El correo ya esta registrado");
     }
 }
